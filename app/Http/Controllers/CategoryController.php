c2d87f9a3695db30947a1123fb6ff72dfb317d21
Sajjad Hossain <sajjadhossain.hs@gmail.com>
2024-07-15 23:32:32 +0600

    product category pagination done

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -16,7 +16,7 @@ class CategoryController extends Controller
     public function index()
     {
         return view('admin.pages.category.index',[
-            'categories' => Category::orderBy('id', 'desc')->get(),
+            'categories' => Category::latest()->paginate(),
         ]);
     }
 
--- resources/views/admin/pages/category/index.blade.php
+++ resources/views/admin/pages/category/index.blade.php
@@ -449,17 +449,10 @@ active
                             </table>
                         </div>
                     </div>
-                    <nav aria-label="Page navigation example">
-                        <ul class="pagination mt-2 mb-2 ml-2">
-                            <li class="page-item prev"><a class="page-link" href="javascript:void(0);">Prev</a></li>
-                            <li class="page-item"><a class="page-link" href="javascript:void(0);">1</a></li>
-                            <li class="page-item"><a class="page-link" href="javascript:void(0);">2</a></li>
-                            <li class="page-item active"><a class="page-link" href="javascript:void(0);">3</a></li>
-                            <li class="page-item"><a class="page-link" href="javascript:void(0);">4</a></li>
-                            <li class="page-item"><a class="page-link" href="javascript:void(0);">5</a></li>
-                            <li class="page-item next"><a class="page-link" href="javascript:void(0);">Next</a></li>
-                        </ul>
-                    </nav>
+
+                    {{--........................ --}}
+                    {{ $categories->links('admin.pages.pagination.paginate') }}
+
                 </div>
             </div>
         </div>
